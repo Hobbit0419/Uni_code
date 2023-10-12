cc9566ea02a229edf66e2e8542a6856662118795
Hobbit0419 <alindbro@bahnhof.se>
2023-10-12 16:34:10 +0200

    Uppgift 2 klar

---
+++ Progg 1/Inlupp 2/Uppgift 2.py
@@ -0,0 +1,27 @@
+from random import randint
+
+def rand_list(length, min, max):
+    l = []
+    for i in range(length):
+        l.append(randint(min, max))
+    return l
+
+
+def smooth(list_a, n):
+    output_list = []
+    for i in range(len(list_a)):
+        if i + 1 + n > len(list_a):
+            output_list.append((sum(list_a[i-n:]) + ((i+n - len(list_a)+1)*list_a[-1]))/(2*n+1))
+            print(list_a[i-n:])
+        elif i - n < 0:
+            output_list.append((sum(list_a[:i+n+1]) + (abs(2*i-n)*list_a[0]))/(2*n+1))
+        else:
+            output_list.append(sum(list_a[i-n:i+n+1])/(2*n+1))
+    return output_list
+
+
+test = [1,2,3,4,5,6,7,8,9]
+smooth_test = smooth(test, 1)
+
+print(test)
+print(smooth_test)
