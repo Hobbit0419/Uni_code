c0caf758d4c6e877d55777d63ac3d0384df20ec3
Hobbit0419 <alindbro@bahnhof.se>
2023-11-21 10:17:27 +0100

    small fix in A

--- Progg 1/Inlupp 4/Uppgift A&B.py
+++ Progg 1/Inlupp 4/Uppgift A&B.py
@@ -7,7 +7,6 @@ def smooth(list_a, n):
     for i in range(len(list_a)):
         if i + 1 + n > len(list_a):
             output_list.append((sum(list_a[i-n:]) + ((i+n - len(list_a)+1)*list_a[-1]))/(2*n+1)) #hanterar randvärden åt höger
-            print(list_a[i-n:])
         elif i - n < 0:
             output_list.append((sum(list_a[:i+n+1]) + (abs(2*i-n)*list_a[0]))/(2*n+1)) #hanterar randvärden åt vänster
         else:
@@ -22,7 +21,7 @@ def csv_reader(file_name):
     for row in data_rows:
         data[str.lower(row[1])] = list(map(float, row[3:]))
     return data
-        
+    
 def data_illustrator(input_data):
     countries = ['dnk', 'fin', 'isl', 'nor', 'swe']
     colors = ['red', 'green', 'yellow', 'blue', 'purple']
