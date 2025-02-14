1e282afa2c78f70a772e0093c8b28973c0aa6043
Hobbit0419 <anton@lindbro.com>
2025-02-14 10:33:40 +0100

    january update

--- algebra 1/inlupp 2/inlupp2.py
+++
@@ -1,8 +0,0 @@
-numbers = [256217472, 9394664064, 3170691216]
-
-for number in numbers:
-    div = 0
-    for i in range(1,number):
-        if number % i == 0:
-            div += 1
-    print(div)
